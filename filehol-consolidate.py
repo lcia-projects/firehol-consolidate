b5bc76a65fc9a6173f1daf5a4edd477c9149599b
darrellmiller <darrellrhodesmiller@gmail.com>
2021-04-01 08:37:58 -0500

    cleaned up code added requirements.txt

--- filehol-consolidate.py
+++ filehol-consolidate.py
@@ -1,22 +1,30 @@
 
+# Louisiana State Police / Louisiana Cyber Investigators Alliance
+# FireHol Processor: downloads the firehol block ipsets and converts them into a single csv and yaml file. This can be
+# used/imported into various tools & network appliances.
+#
+# Resources:
+# http://la-safe.org
+# https://github.com/lcia-projects
+# http://iplists.firehol.org/
+
+
 import os
 from git import Git
 import shutil
 from pprint import pprint
 
 ipDict={}
+dirpath = './blocklist-ipsets'
 
 def pullListsFromGit():
     try:
         print("--: Pulling new datasets from GIT, this could take a minute or two, please wait..")
-
         # remove previous data
-        dirpath = './blocklist-ipsets'
+        if os.path.isdir(dirpath):
+            shutil.rmtree(dirpath)
 
-        try:
-            os.makedirs(dirpath)
-        except OSError as e:
-            print ("Data Directory not found, creating it")
+        os.mkdir(dirpath)
 
         # pull new data
         print("----: Getting new data Lists.....")
@@ -37,18 +45,15 @@ def readFiles(filename):
                 strDescription = strDescription.replace(".ipset", "")
                 strDescription = strDescription.replace("./blocklist-ipsets/", "")
                 line = line.replace("\n", "")
-                # print(line, ":", filename, ":", strDescription)
                 if line in ipDict.keys():
                     # already in dictionary
                     ipDict[line].append(strDescription)
-                    # print ("---- Duplicate:", self.ipDict[line])
 
                 else:
                     # new ip
                     tagArray.append(strDescription)
                     ipDict[line] = tagArray.copy()
                     tagArray.clear()
-                    # print ("New IP added:", self.ipDict[line])
 # for testing
 def viewData():
     pprint(ipDict)
@@ -64,6 +69,8 @@ def saveYAML():
 def saveCSV():
     print("--: Saving CSV File Please Wait.. ")
     csvWriter = open("firehol_consolidated.csv", "w")
+
+    #cleans up and saves output
     for item in ipDict:
         csvTags=str(ipDict[item])
         csvTags=csvTags.replace('[','')
@@ -76,17 +83,18 @@ def saveCSV():
 if __name__ == '__main__':
     dataSet={}
 
-    #fireHolProcessor = libFireHol.fireHolProcssorv1(dataSet)
-
     # pull iplists from git
     pullListsFromGit()
 
-    #get files
+    #get file names and process them
     for file in os.listdir("./blocklist-ipsets"):
         if file.endswith(".ipset"):
             strFilename=os.path.join("./blocklist-ipsets", file)
             readFiles(strFilename)
 
-    # viewData()
     saveYAML()
     saveCSV()
+
+    # removes ipset data
+    if os.path.isdir(dirpath):
+        shutil.rmtree(dirpath)
